86a847a02e41987f5cdd58ac14f4ef9f11c3eb8b
Atul Kishor <atul.kishor@netradyne.com>
2025-07-01 15:58:58 +0530

    add subprocess

--- datascience/service/my_serv.py
+++ datascience/service/my_serv.py
@@ -1,4 +1,7 @@
 
 def my_serv():
     # This is a placeholder function for my_serv.py
+    with open("my_serv.log", "w") as log_file:
+        log_file.write("Service started successfully.\n")
+    print("Service started successfully.")
     return 4
--- datascience/testing/testcases/test_my_serv.py
+++ datascience/testing/testcases/test_my_serv.py
@@ -1,3 +1,31 @@
+import os
+import signal
+import subprocess
+import shlex
+from pathlib import Path
 
-def test_my_serv():
-    pass
+def kill_pid(pid):
+    """
+     Kills the pid
+    :param pid: process id
+    :return: True
+    """
+    try:
+        os.kill(int(pid), signal.SIGKILL)
+        print("PID {} killed successfully".format(pid))
+    except OSError:
+        print("No processes was running or Permission denied or Process killed internally")
+    return True
+
+def test_my_serv():            
+    base_dir = Path(__file__).resolve().parent.parent.parent
+    cmargs = shlex.split("python3 my_serv.py")
+    service_path = os.path.join(base_dir, 'service')
+    os.chdir(service_path)
+    print(os.getcwd())
+    result = subprocess.Popen(cmargs)
+    pid = result.pid
+    st = "pid is {pid}".format(pid=pid)
+    print(st)
+    print(os.getcwd())
+    kill_pid(pid)
